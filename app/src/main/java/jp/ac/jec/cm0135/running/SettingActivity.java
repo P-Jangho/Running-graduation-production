0abd9acdf5dd8f0becac73e6ba98d0faf7574224
P-Jangho <wkdgh778123@gmail.com>
2023-12-05 15:07:31 +0900

    Add a time spinner

---
+++ app/src/main/java/jp/ac/jec/cm0135/running/SettingActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.jec.cm0135.running;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
