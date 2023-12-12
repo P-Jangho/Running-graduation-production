40e5b8624144beff9e04c8b7c0f34c4d8eb0e7b7
P-Jangho <wkdgh778123@gmail.com>
2023-12-12 15:11:14 +0900

    Add the ability to obtain current location information and weather information

--- app/src/main/java/jp/ac/jec/cm0135/running/MainActivity.java
+++ app/src/main/java/jp/ac/jec/cm0135/running/MainActivity.java
@@ -1,20 +1,39 @@
 package jp.ac.jec.cm0135.running;
 
+import static jp.ac.jec.cm0135.running.BuildConfig.MY_KEY;
+
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +42,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView progressText;
     int i = 0;
 
-    private Button btnP;
     private Button btnStart;
     private ImageButton btnSet;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView locationTextView;
+    private TextView weatherInfoTextView; // 추가된 부분
+    private static final String API_KEY = MY_KEY;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -39,41 +63,24 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
         progressText = findViewById(R.id.progress_text);
 
-//        btnP = findViewById(R.id.btnP);
         btnStart = findViewById(R.id.btnStart);
         btnSet = findViewById(R.id.btnSet);
 
-//        final Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                // set the limitations for the numeric
-//                // text under the progress bar
-//                if (i <= 100) {
-//                    progressText.setText("" + i);
-//                    progressBar.setProgress(i);
-//                    i++;
-//                    handler.postDelayed(this, 200);
-//                } else {
-//                    handler.removeCallbacks(this);
-//                }
-//            }
-//        }, 200);
+        locationTextView = findViewById(R.id.locationTextView);
+        weatherInfoTextView = findViewById(R.id.weatherInfoTextView); // 추가된 부분
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-//        btnP.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (i <= 100) {
-//                    progressText.setText("" + i);
-//                    progressBar.setProgress(i);
-//                    i++;
-//                } else {
-//                    i = 0;
-//                    progressText.setText("" + i);
-//                    progressBar.setProgress(i);
-//                }
-//            }
-//        });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없는 경우 권한을 요청합니다.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // 권한이 있는 경우 위치 업데이트를 시작합니다.
+            requestLocationUpdates();
+            new WeatherTask().execute();
+        }
 
         // 현재 날짜 가져오기
         Calendar calendar = Calendar.getInstance();
@@ -108,4 +115,111 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void requestLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        // 위치를 가져와서 텍스트뷰에 표시합니다.
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        String locationText = "위도: " + latitude + "\n경도: " + longitude;
+                        locationTextView.setText(locationText);
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 승인되면 위치 업데이트를 시작합니다.
+                requestLocationUpdates();
+            } else {
+                // 권한이 거부되면 처리할 내용을 추가할 수 있습니다.
+            }
+        }
+    }
+
+    private class WeatherTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                // 위치 정보를 가져와서 API에 요청하는 코드
+                double latitude = 35.6989;
+                double longitude = 139.6967;
+                String apiUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+                        + latitude + "," + longitude
+                        + "?unitGroup=metric&include=days%2Ccurrent&key=" + API_KEY + "&contentType=json";
+
+                URL url = new URL(apiUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                try {
+                    // API 응답을 읽어오기
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+
+                    // JSON 파싱
+                    String jsonResult = stringBuilder.toString();
+                    parseJson(jsonResult);
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("WeatherActivity", "Error fetching weather data", e);
+            }
+            return null;
+        }
+
+        private void parseJson(String json) {
+            try {
+                // JSON을 JsonObject로 파싱
+                JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+                // "days" 배열 가져오기
+                JsonArray daysArray = jsonObject.getAsJsonArray("days");
+
+                if (daysArray != null && daysArray.size() > 0) {
+                    // "days" 배열의 첫 번째 요소 가져오기
+                    JsonObject firstDayObject = daysArray.get(0).getAsJsonObject();
+
+                    // "temp"과 "icon" 필드 추출
+                    double temp = firstDayObject.getAsJsonPrimitive("temp").getAsDouble();
+                    String icon = firstDayObject.getAsJsonPrimitive("icon").getAsString();
+
+                    // UI 업데이트를 메인 스레드에서 수행
+                    runOnUiThread(() -> {
+                        // 날씨 정보를 TextView에 표시
+                        String weatherInfo = "날씨: " + icon + "\n온도: " + temp + "°C";
+                        weatherInfoTextView.setText(weatherInfo);
+                    });
+                } else {
+                    Log.e("WeatherActivity", "No data available in 'days' array");
+                }
+            } catch (Exception e) {
+                Log.e("WeatherActivity", "Error parsing JSON", e);
+            }
+        }
+
+    }
+
 }
